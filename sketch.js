e3a7c794a59ad2441d7219ddef21e4eaa29ccb42
bijucksae <jiwonmin000@gmail.com>
2024-12-06 17:53:38 +0900

    sketch 수정

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let fireworks = [];
+let fireworkCount = 0;
+let maxFireworks = 5;
+let fireworkSpacing;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  background(0);
+  fireworkSpacing = width / maxFireworks;
 }
 
 function draw() {
-  background(220);
+  background(0, 50);
+  
+  for (let i = fireworks.length - 1; i >= 0; i--) {
+    let fw = fireworks[i];
+    fw.update();
+    fw.display();
+    
+    if (fw.isDone()) {
+      fireworks.splice(i, 1);
+    }
+  }
+
+  if (fireworkCount < maxFireworks && frameCount % 60 === 0) {
+    let fireworkPosX = fireworkSpacing * fireworkCount + fireworkSpacing / 2; // 각 불꽃의 x 위치 계산
+    fireworks.push(new Firework(fireworkPosX));
+    fireworkCount++;
+  }
 }
